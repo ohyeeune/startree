fdadb96153dec5f50916ae69169975a42aa217b8
ohyeeune <s2018s33@e-mirim.hs.kr>
2020-05-12 12:41:51 +0900

    피라미드 별 찍기

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,6 +10,19 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            int num = 8;
+            for(int i = 0; i<num; i++)
+            {
+                for(int j = num-1; j>i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for(int j =0; j<i*2+1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
